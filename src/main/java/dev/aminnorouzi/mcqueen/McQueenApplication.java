68324ca67c19bf0872697dc6827d063384314e8d
aminnorouzi <aminnorouzidev@gmail.com>
2023-11-06 23:09:48 +0330

    Created the project

---
+++ src/main/java/dev/aminnorouzi/mcqueen/McQueenApplication.java
@@ -0,0 +1,13 @@
+package dev.aminnorouzi.mcqueen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McQueenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McQueenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aminnorouzi/mcqueen/McQueenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aminnorouzi.mcqueen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McQueenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
