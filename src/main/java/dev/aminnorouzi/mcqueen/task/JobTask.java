454d827056ab1818d4ebd05825e3052cf314e096
aminnorouzi <aminnorouzidev@gmail.com>
2023-11-09 20:56:57 +0330

    Refactored the rss client code

--- src/main/java/dev/aminnorouzi/mcqueen/task/JobTask.java
+++ src/main/java/dev/aminnorouzi/mcqueen/task/JobTask.java
@@ -2,11 +2,12 @@ package dev.aminnorouzi.mcqueen.task;
 
 import dev.aminnorouzi.mcqueen.client.JobClient;
 import dev.aminnorouzi.mcqueen.event.NotificationEvent;
-import dev.aminnorouzi.mcqueen.model.rss.Item;
 import dev.aminnorouzi.mcqueen.model.job.Job;
+import dev.aminnorouzi.mcqueen.model.rss.Item;
 import dev.aminnorouzi.mcqueen.model.rss.Rss;
 import dev.aminnorouzi.mcqueen.service.JobService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -21,16 +22,21 @@ public class JobTask {
 
     private final JobClient jobClient;
     private final JobService jobService;
-    private final ApplicationEventPublisher applicationEventPublisher; // q=java+spring
-                                                                        // sort=recency hourly_rate=20-
-    @Scheduled(cron = "0 */1 * * * ?")                                  // job_type=hourly verified_payment_only=1
-    public void getJobsFromRss() {
-        List<Item> items;
-        try {
-            Rss rss = jobClient.getJobs("");
-            items = new ArrayList<>(rss.getItems()); // get all jobs with the filters (list of filters)
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Value("${rss.client.url}")
+    private String url;
+
+    @Value("#{'${rss.client.keywords}'.split(',')}")
+    private List<String> keywords;
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void getJobsFromRss() throws URISyntaxException {
+        List<Item> items = new ArrayList<>();
+
+        for (String keyword : keywords) {
+            Rss rss = jobClient.getJobs(url.formatted(keyword));
+            items.addAll(rss.getItems());
         }
 
         List<Job> jobs = jobService.separate(items);
